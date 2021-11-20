466ed723cc07fa56fa1caaa60de4aeac7d3df95a
Josip-github <josipcota1@gmail.com>
2021-11-20 07:48:23 +0100

    This commit creates getCounterPage() method Its duty is to return counter.html, but it doesn't work at the moment because in the template there are some thymeleaf atrributes which have to be defined in backend as well

--- src/main/java/com/example/usertestingapplication/CounterController.java
+++ src/main/java/com/example/usertestingapplication/CounterController.java
@@ -1,6 +1,8 @@
 package com.example.usertestingapplication;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,4 +12,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CounterController {
 
     private AtomicInteger count = new AtomicInteger(0);
+
+    @GetMapping
+    public String getCounterPage(Model model){
+        return "counter";
+    }
 }
