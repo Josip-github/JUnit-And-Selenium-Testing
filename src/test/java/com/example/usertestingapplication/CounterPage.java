9a6d9884a1c734286346d5f315f7ec945394b795
Josip-github <josipcota1@gmail.com>
2021-11-20 09:02:08 +0100

    This commit adds comments for better understanding These IDs in CounterPage class match the IDs in the template

--- src/test/java/com/example/usertestingapplication/CounterPage.java
+++ src/test/java/com/example/usertestingapplication/CounterPage.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.support.FindBy;
 
 public class CounterPage {
 
-    @FindBy(id = "count-display")
+    @FindBy(id = "count-display") // counter.html line 13
     private WebElement countDisplay;
 
-    @FindBy(id = "increment-button")
+    @FindBy(id = "increment-button") // counter.html line 17
     private WebElement incrementButton;
 
-    @FindBy(id = "reset-value-field")
+    @FindBy(id = "reset-value-field") // counter.html line 22
     private WebElement resetValueField;
 
-    @FindBy(id = "reset-button")
+    @FindBy(id = "reset-button") // counter.html line 23
     private WebElement resetButton;
 
 }
