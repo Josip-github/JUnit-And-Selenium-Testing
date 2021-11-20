07ddc2add5e631bc88c51502f553cacb8cf50969
Josip-github <josipcota1@gmail.com>
2021-11-20 17:09:32 +0100

    This commit defines fields of the class and beforeAll and afterAll methods Here we define static WebDriver field that is initialized in our beforeAll() method and quit in our afterAll() method. This is a classic example why JUnit and Selenium are a very good match and why they fit greatly with each other.

--- src/test/java/com/example/usertestingapplication/UserTestingApplicationTests.java
+++ src/test/java/com/example/usertestingapplication/UserTestingApplicationTests.java
@@ -1,6 +1,11 @@
 package com.example.usertestingapplication;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
@@ -9,6 +14,20 @@ class UserTestingApplicationTests {
 
     @LocalServerPort
     private Integer port;
+
+    private static WebDriver driver;
+    private CounterPage counter;
+
+    @BeforeAll
+    public static void beforeAll(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        driver.quit();
+    }
 
     @Test
     void contextLoads() {
