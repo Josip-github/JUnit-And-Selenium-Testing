3731c788682b3acd7a920d82d047a07a52bfef1f
Josip-github <josipcota1@gmail.com>
2021-11-20 07:31:24 +0100

    Initial commit Dependencies that I need for this project are: thymeleaf, spring web, spring devtools, selenium, webdrivermanager

---
+++ src/test/java/com/example/usertestingapplication/UserTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usertestingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
