130fd5da8cde59612a9823e30637c9a8720a1396
Josip-github <josipcota1@gmail.com>
2021-11-20 16:53:15 +0100

    This commit does a bit of additional setup concerning testing @SpringBootTest annotation allows Spring to start our application when the tests are run. Argument to the annotation specifies that the server should be run on a random port. We don't need to know specifically which port that is, but Spring allows us to inject it into this class using the @LocalServerPort annotation. Having it injected we can later use for whatever we need.

--- src/test/java/com/example/usertestingapplication/UserTestingApplicationTests.java
+++ src/test/java/com/example/usertestingapplication/UserTestingApplicationTests.java
@@ -2,9 +2,13 @@ package com.example.usertestingapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserTestingApplicationTests {
+
+    @LocalServerPort
+    private Integer port;
 
     @Test
     void contextLoads() {
