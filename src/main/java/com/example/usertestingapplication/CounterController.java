44c123d5c39f83f958e2c5843ac460c0133d09ac
Josip-github <josipcota1@gmail.com>
2021-11-20 07:56:07 +0100

    This commit creates initResetForm() method This method binds th:object="${resetForm} to the ResetForm class in backend. Only now the counter.html is being displayed with no problems

--- src/main/java/com/example/usertestingapplication/CounterController.java
+++ src/main/java/com/example/usertestingapplication/CounterController.java
@@ -3,6 +3,7 @@ package com.example.usertestingapplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -16,5 +17,12 @@ public class CounterController {
     @GetMapping
     public String getCounterPage(Model model){
         return "counter";
+    }
+
+    @ModelAttribute("resetForm")
+    public ResetForm initResetForm(){
+        ResetForm result = new ResetForm();
+        result.setValue(0);
+        return result;
     }
 }
